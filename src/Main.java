9d2eaef66a79599ad1c46dc9333cbc343cb6a843
Alexey Elizarov <haakee@gmail.com>
2020-11-04 09:28:27 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
         staff[0] = new Employee("Кузьма", "Домовой", "fancy_kuzma@gmail.com", "+7 343 667 666", 100.15f, 115);
         staff[1] = new Employee("Фрекен Бок", "Хозяйка", "mamka_s_youtuba@gmail.com", "+7 343 546 782", 500.15f, 45);
         staff[2] = new Employee("Карлсон", "Трубочист", "chistiy_dymohod@gmail.com", "+7 343 898 214", 150.15f, 40);
-        staff[3] = new Employee("Мойдодыр", "Баньщик", "anzor_sauna@gmail.com", "+7 343 872 012", 300.15f, 35);
+        staff[3] = new Employee("Мойдодыр", "Баньщик", "anzor_sauna@gmail.com", "+7 343 872 014", 300.15f, 35);
         staff[4] = new Employee("Алёнушка", "Хостесс", "tyan_na_maximalkah@gmail.com", "+7 343 696 969", 250.15f, 25);
+
+        System.out.print("dasfdsfsdf");
 
         for (int i = 0; i < staff.length; i++) {
             if (staff[i].age < 40) {
